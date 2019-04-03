8f1687b0bf1e997fd79d4cb851bcf0961d3b15c1
Caleb Herod <cmherod@uchicago.ediu>
2019-04-03 11:58:55 -0500

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -1,3 +1,10 @@
 if True:
-  print("Hello world")
-  print("Bye Girl!")
+    print("Hello world")
+    print("Bye Girl!")
+x = 4
+y = 7
+print(x + y)
+
+print("oh yeah, more work")
+
+print("this class is great")
